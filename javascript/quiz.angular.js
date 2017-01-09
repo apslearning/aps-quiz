b749c6b2768fe3b179d3aa52e4a411070c95ab6e
Bhagirath Kumar <bhagirathrajrappa@gmail.com>
2017-01-09 19:55:12 +0530

    User Registration Page Updated

--- javascript/quiz.angular.js
+++ javascript/quiz.angular.js
@@ -1,13 +1,19 @@
+$(document).ready(function(){
+	alert("hi");	
+});
 var app = angular.module("myApp", []);
 
 app.controller("myController", function($scope,$http) {
+	/* Application login functionality */
 	$scope.validate=function(user){		
 		$http.get("http://www.w3schools.com/angular/customers_sql.aspx")
 	    .then(function (response) {$scope.names = response.data.records;});
 	}
-	$scope.resetValidate=function(user){
-		user.userid="";
-		user.password="";
+	
+	/* Application Registration functionality */
+	$scope.userRegistration = function(reguser){
+				
+		
 	}
     
 });
